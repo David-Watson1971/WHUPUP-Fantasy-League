860e5b2156135e945dbb346fba1d49a165826043
David Watson <davidwatson44@hotmail.com>
2024-09-12 09:52:18 +0100

    Update whu_tang_clan.py to be the last page

--- pages/whu_tang_clan.py
+++ pages/whu_tang_clan.py
@@ -9,7 +9,7 @@ from dash.dependencies import Input, Output
 template_theme2 = "flatly"
 template_theme1 = "darkly"
 
-dash.register_page(__name__, path='/whu_tang_clan', name="WHU-Tang-Clan")
+dash.register_page(__name__, path='/whu_tang_clan', name="WHU-Tang-Clan", order=4)
 
 
 df = pd.read_excel('WHUPUP.xlsx', sheet_name='Weekly Points')
